b646244e693d65b3cab26edb4630826fcf99b7c9
Frederick.Yap@fujixerox.com <Frederick.Yap@fujixerox.com>
2013-02-26 13:04:28 +0800

    Configure JSF setup.

---
+++ framework-ui/src/main/java/framework/ui/databeans/AbstractDatabean.java
@@ -0,0 +1,10 @@
+package framework.ui.databeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@SessionScoped
+@ManagedBean
+public class AbstractDatabean {
+
+}
